f63e590f965fedf8bf467259f0e47f8f7cb2c278
elicas17 <73227850J@CEBANCDEA.EDU>
2021-01-21 20:24:53 +0100

    inici

--- js/juego.js
+++ js/juego.js
@@ -1,14 +1,14 @@
-
-
 window.onload = load();
-
-
-
-var fechaHora = new Date();
-document.getElementById("tiempo").innerHTML = fechaHora;
-
 function load(){
-    
+
+
+    var fechaHora = new Date();
+    document.getElementById("tiempo").innerHTML = fechaHora.getHours() +":"+fechaHora.getMinutes()+":"+fechaHora.getSeconds();
+
+
+    var tiempoInicio=fechaHora.getHours() +":"+fechaHora.getMinutes()+":"+fechaHora.getSeconds();
+    console.log(tiempoInicio)
+
 
     var comprar=document.getElementById("comprar");
     var vender=document.getElementById("vender");
